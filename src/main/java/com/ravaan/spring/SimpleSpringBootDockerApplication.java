0f3176b4fc5d2787cbd91dac67ba1ab25aded5b2
Ravaan Techky <ravaan.techky@gmail.com>
2020-07-18 06:24:20 +0530

    Simple Spring Boot Project

---
+++ src/main/java/com/ravaan/spring/SimpleSpringBootDockerApplication.java
@@ -0,0 +1,13 @@
+package com.ravaan.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravaan/spring/SimpleSpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravaan.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
